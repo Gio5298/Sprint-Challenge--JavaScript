af00e71ab09218d785f9831d37fca953a9c00610
Regino P <regino5298@gmail.com>
2019-10-18 11:22:29 -0700

    function, step 1, 2, & 3 including closures complete

--- challenges/classes.js
+++ challenges/classes.js
@@ -8,35 +8,17 @@ class CuboidMaker {
     }// methods go below here vvv
     volume(){ 
         return this.length * this.width * this.height;
-    }
+        }
     surfaceArea(){ 
-        // () => {
-            /*surface =*/ 
-            return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
         }
     }
-
-//===^1^===
-
-// CuboidMaker.prototype.volume = function(length,width,height){
-
-// return this.length * this.width * this.height;
-// }
-//===^2^===
-
-// CuboidMaker.prototype.surfaceArea = function(length,width,height){
-
-// surface = 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
-//   return surface
-//}
-//===^3^===
 
 const cuboid = new CuboidMaker({
 length: 4,
 width: 5,
 height: 5,
 });
-//===^4^===
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
